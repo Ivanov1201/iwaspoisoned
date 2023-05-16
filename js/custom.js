3b92d25dd7bc8fa2e4e2bebcc428f4501b83c6a0
Ivanov1201 <vitaliy.ivanov1201@gmail.com>
2023-05-16 17:22:10 +0300

    replace the main site

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function() {
     formSlider.init('#example-with-groups');
 });
+
+$(document).ready(function(){ 
+    $(".sick-from").click(function(){
+        $('.not-sure').removeClass("active");
+        $(this).addClass("active");
+        $(".place").attr("placeholder","Where? e.g. Moe's Tavern");
+    });
+    $(".not-sure").click(function(){
+        $('.sick-from').removeClass("active");
+        $(this).addClass("active");
+        $(".place").attr("placeholder","Type your city and state...");
+    });
+});
